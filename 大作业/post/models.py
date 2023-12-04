0d17afc7ed2f3948ff7660fbb4442c26f36a8dbd
LanYuLong666 <112047062+LanYuLong666@users.noreply.github.com>
2023-12-04 17:06:48 +0800

    1

--- 大作业/post/models.py
+++ 大作业/post/models.py
@@ -4,3 +4,25 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    cname = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return "Category: %s"% self.cname
+class Tag(models.Model):
+    tname = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return "Tag: %s" % self.tname
+class Post(models.Model):
+    title = models.CharField(max_length=70, unique=True)
+    desc = models.CharField(max_length=200, blank=True,null=True)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return "Post: %s" % self.title
+    class Meta:
+        ordering = ['-created']
